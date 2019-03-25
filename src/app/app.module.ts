428206b16e97a0f1945c6308b41263f800fa0beb
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-03-25 14:22:15 +0200

    #1 project preparing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
