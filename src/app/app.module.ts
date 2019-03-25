08dae01dd7e6e55515c32a6294c6e6b28d3f3060
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-03-25 18:04:52 +0200

    #4 twoway binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ProductComponent } from './product/product.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProductsComponent {
 
   addProductStatus = '';
-  inputText = '';
+  inputText = 'Default text';
 
   constructor() {
   }
