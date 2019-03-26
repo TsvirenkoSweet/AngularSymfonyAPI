bfe04fd7340f7b2a569d133dc27ea36e7764d99b
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-03-26 12:26:53 +0200

    #6 ngFor

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,14 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent {
 
-  carName = '';
+  productTitle = '';
 
   addProductStatus = false;
+
+  products = ['Apple', 'Samsung', 'Lenovo'];
+
+  items = [
+    {
+      id: 11,
+      name: 'item 1'
+    },
+    {
+      id: 233,
+      name: 'item 2'
+    },
+    {
+      id: 32,
+      name: 'item 3'
+    },
+    {
+      id: 441,
+      name: 'item 4'
+    },
+    {
+      id: 5,
+      name: 'item 5'
+    },
+    {
+      id: 622,
+      name: 'item 6'
+    }
+  ];
 
   constructor() {
   }
 
   addProduct() {
     this.addProductStatus = true;
+    this.products.push(this.productTitle);
+    this.productTitle = '';
   }
 }
