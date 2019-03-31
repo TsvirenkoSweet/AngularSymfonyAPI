5145226ba0e24f326673ecf558069498caa9fac0
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-04-01 02:06:16 +0300

    #17 hostlistener and hostbinding

--- src/app/directives/bg.directive.ts
+++ src/app/directives/bg.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 
 @Directive({
@@ -7,12 +7,22 @@ import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 export class BgDirective implements OnInit {
 
+  @HostBinding('style.backgroundColor') background: string;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'grey');
-    this.renderer.addClass(this.element.nativeElement, 'green-text');
     // this.element.nativeElement.style.backgroundColor = 'red';
+  }
+
+  @HostListener('mouseenter') mouseEnter() {
+    this.background = 'red';
+    // this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'white');
+  }
+
+  @HostListener('mouseleave') mouseLeave() {
+    this.background = 'grey';
+    // this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'grey');
   }
 
 }
