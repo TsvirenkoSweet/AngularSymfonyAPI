2b28ed29270a1c8158a6bbb065511f640326bbed
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-04-01 02:17:03 +0300

    #18 params inside directives

--- src/app/directives/bg.directive.ts
+++ src/app/directives/bg.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 
 @Directive({
@@ -7,21 +7,25 @@ import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} fro
 
 export class BgDirective implements OnInit {
 
+  @Input('appBg') hoverColor: string = 'green';
+  @Input() defaultColor: string = 'transparent';
+
   @HostBinding('style.backgroundColor') background: string;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.background = this.defaultColor;
     // this.element.nativeElement.style.backgroundColor = 'red';
   }
 
   @HostListener('mouseenter') mouseEnter() {
-    this.background = 'red';
+    this.background = this.hoverColor;
     // this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'white');
   }
 
   @HostListener('mouseleave') mouseLeave() {
-    this.background = 'grey';
+    this.background = this.defaultColor;
     // this.renderer.setStyle(this.element.nativeElement, 'backgroundColor', 'grey');
   }
 
