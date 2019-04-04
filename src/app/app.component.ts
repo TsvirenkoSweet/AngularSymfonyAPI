579df86b9d0b2bb5973301ba53747d6fbfea13c7
TsvirenkoSweet <andrei.tsvirenko@gmail.com>
2019-04-04 14:36:09 +0300

    #26 set-up fake json api server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit {
-  answers = [{
-    type: 'yes',
-    text: 'Да'
-  }, {
-    type: 'no',
-    text: 'Нет'
-  }];
-
-  charsCount = 5;
-
-  form: FormGroup;
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email], this.checkForEmail),
-        pass: new FormControl('', [Validators.required, this.checkForLength.bind(this)]),
-      }),
-      country: new FormControl('ru'),
-      answer: new FormControl('no')
-    });
-  }
-
-  onSubmit() {
-    console.log('Submited!', this.form);
-  }
-
-  checkForLength(control: FormControl) {
-    if (control.value.length <= this.charsCount) {
-      return {
-        lengthError: true
-      };
+export class AppComponent {
+  cars = [
+    {
+      name: 'Ford',
+      color: 'white',
+      id: 1
     }
-    return null;
-  }
-
-  checkForEmail(control: FormControl): Promise<any> {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (control.value === 'test@mail.ru') {
-          resolve({
-            emailIsUsed: true
-          });
-        } else {
-          resolve(null);
-        }
-      }, 3000);
-    });
-  }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
